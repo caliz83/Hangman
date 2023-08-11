2e272b28a7164c7f72b4f51e070a33f15e2b6442
caliz83 <lizg1308@gmail.com>
2023-08-10 20:45:58 -0700

    first commit

---
+++ Hangman/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  
+  const [wordToGuess, setWordToGuess] = useState('Test')
+
+  return <h1>blah</h1>
+}
+
+export default App
