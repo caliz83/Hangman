7ed65b8db1d09d5a6f5f156044f2423290b763af
caliz83 <lizg1308@gmail.com>
2023-08-13 20:51:30 -0700

    hangman drawing done; broke app return in main :(

--- Hangman/src/App.tsx
+++ Hangman/src/App.tsx
@@ -1,31 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import HangmanDrawing from './HangmanDrawing';
-import HangmanWord from './HangmanWord';
-import Keyboard from './Keyboard';
-
+import { useState } from "react";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 
 function App() {
-  
-  const words = 'tbd';
+  const words = "tbd"; //words to pull from api - figure it out and delete this declaration later
   const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  }) //words to pull from api - figure it out
+    return words[Math.floor(Math.random() * words.length)];
+  });
 
-  console.log(wordToGuess);
+  //console.log(wordToGuess);
 
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]); //each string only length of 1
 
-  return 
-  <div>
-    <div>
-    Lose
-    Win
-    </div>
+  return
+  <div style={{maxWidth: '800px', display: 'flex', flexDirection: 'column', gap: '2rem', margin: '0 auto', alignItems: 'center' }}>
+    <div style={{ fontSize: '2rem', textAlign: 'center' }}>Lose Win</div>
     <HangmanDrawing />
     <HangmanWord />
     <Keyboard />
-  </div>
+  </div>;
 }
 
-export default App
+export default App;
